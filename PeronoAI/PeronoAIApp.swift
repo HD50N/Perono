b735876ddac1c95a382cf27108ba565ab346ccf7
Hudson Chung <67850085+HD50N@users.noreply.github.com>
2025-01-18 14:06:40 -0600

    Initial Commit

---
+++ PeronoAI/PeronoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeronoAIApp.swift
+//  PeronoAI
+//
+//  Created by Hudson Chung on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct PeronoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
